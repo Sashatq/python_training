0ab9be42e356c777d46d37799c36ef03c25d41b4
Sashatq <40859524+Sashatq@users.noreply.github.com>
2018-08-21 12:32:46 +0300

    Реализован метод кэширования и хеширования для тестов

--- fixture/group.py
+++ fixture/group.py
@@ -22,6 +22,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -52,6 +53,7 @@ class GroupHelper:
         # submit modify
         wd.find_element_by_name("update").click()
         self.return_to_group()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -60,6 +62,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group()
+        self.group_cache = None
 
     def return_to_group(self):
         wd = self.app.wd
@@ -73,13 +76,16 @@ class GroupHelper:
         len_group = len(groups)
         return len_group
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.groups_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.groups_cache.append(Group(name=text, id=id))
+        return list(self.groups_cache)
 
