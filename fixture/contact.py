120a916bed4a407395146ecc8045ab8ab140fd36
Sashatq <40859524+Sashatq@users.noreply.github.com>
2018-08-23 18:13:45 +0300

    модифицирован тест по удалению контакта
    Удаляется рандомный контакт

--- fixture/contact.py
+++ fixture/contact.py
@@ -51,6 +51,10 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def fill_form(self, contact):
         self.change_field_value("firstname", contact.name)
         self.change_field_value("middlename", contact.mname)
@@ -87,13 +91,17 @@ class ContactHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def delete_first_contact(self):
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.open_contact_page()
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
         self.contact_cache = None
+
+    def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
 
     def edit_first_contact(self, new_group_data):
         wd = self.app.wd
--- test/test_delete_first_contact.py
+++ test/test_delete_first_contact.py
@@ -1,8 +1,15 @@
 from model.contact import Contact
+from random import randrange
 
 
 def test_delete_first_contact(app):
     count = app.contact.count()
     if count == 0:
         app.contact.create(Contact(name="for_delete", mname="for_delete", lname="for_delete", nick="for_delete"))
-    app.contact.delete_first_contact()
+    old_contact = app.contact.get_contact_list()
+    index = randrange(len(old_contact))
+    app.contact.delete_contact_by_index(index)
+    new_contact = app.contact.get_contact_list()
+    assert len(old_contact) - 1 == len(new_contact)
+    old_contact[index:index+1] = []
+    assert old_contact == new_contact
