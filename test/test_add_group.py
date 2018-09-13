afeb9521628ebc66a5827a69b3226d4f9e0fc2d2
Sashatq <40859524+Sashatq@users.noreply.github.com>
2018-09-13 15:51:25 +0300

    Опции для фикстуры

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,13 +10,13 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
+test_data = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
     for i in range(5)
 ]
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
 def test_test_add_group(app, group):
     old_groups = app.group.get_group_list()
     app.group.create(group)
