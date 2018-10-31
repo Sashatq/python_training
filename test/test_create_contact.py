579a4b0bac7d45933b51474400f50568c4c35bfe
Sashatq <40859524+Sashatq@users.noreply.github.com>
2018-10-31 17:07:14 +0300

    Параметры отдельным файлом

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -18,9 +18,8 @@ test_data = [Contact(lname=random_string("l", 8), name=random_string("n", 4), wo
 def test_create_contact(app, contact):
     old_contact = app.contact.get_contact_list()
     app.contact.create(contact)
+    time.sleep(2)
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) + 1 == len(new_contact)
     old_contact.append(contact)
-    sort_old = sorted(old_contact, key=Contact.id_or_max)
-    sort_new = sorted(new_contact, key=Contact.id_or_max)
-    assert sort_old == sort_new
+    assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
