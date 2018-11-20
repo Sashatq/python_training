b0431c09127ed6ea24f2d3ca6c2ad2c4f7c1c363
Sashatq <40859524+Sashatq@users.noreply.github.com>
2018-11-20 11:32:13 +0300

    Доработан тест по созданию контакта
    ui проверка выключена, списки сравниваются на основе базы данных

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,11 +1,10 @@
 from model.contact import Contact
 
 
-def test_create_contact(app, json_contacts):
+def test_create_contact(app, db, json_contacts):
     contact = json_contacts
-    old_contact = app.contact.get_contact_list()
+    old_contact = db.get_contact_list()
     app.contact.create(contact)
-    new_contact = app.contact.get_contact_list()
-    assert len(old_contact) + 1 == len(new_contact)
+    new_contact = db.get_contact_list()
     old_contact.append(contact)
     assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
